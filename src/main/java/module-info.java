e39c9eea9ed7ebec3cc6270aaa4e2d08ac3e5eba
Rob Whitnell <rwhitnel@guilford.edu>
2025-03-10 09:52:08 -0400

    add transitive directive to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module edu.guilford {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
+    requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens edu.guilford to javafx.fxml;
     exports edu.guilford;
